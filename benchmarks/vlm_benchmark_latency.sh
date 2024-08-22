ab564df9f591f59f033ee97581f6b32f19011e13
xuhao <runordie@qq.com>
2024-08-22 11:45:27 +0800

    feat:update benchmakr test oaip

--- benchmarks/vlm_benchmark_latency.sh
+++ benchmarks/vlm_benchmark_latency.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
+# 检查参数
+if [ "$#" -ne 1 ]; then
+    echo "usage: bash $0 <测试模型路径>"
+    exit 1
+fi
 root=$(dirname $(dirname $(realpath $0)))
+model=$1
+echo "test model: $model"
+rm -f $root/latency-*.json
 
-rm $root/latency-*.json
-CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_latency.py --batch 1 --input-len 512 --output-len 128   --output-json latency-1-512-128.json
-CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_latency.py --batch 1 --input-len 512 --output-len 1024  --output-json latency-1-512-1024.json
-CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_latency.py --batch 1 --input-len 2048 --output-len 128  --output-json latency-1-2048-128.json
-CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_latency.py --batch 1 --input-len 2048 --output-len 1024 --output-json latency-1-2048-1024.json
-CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_latency.py --batch 1 --input-len 4096 --output-len 128  --output-json latency-1-4096-128.json
-CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_latency.py --batch 1 --input-len 4096 --output-len 1024 --output-json latency-1-4096-1024.json
+export VLLM_WORKER_MULTIPROC_METHOD=spawn
 
-CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_latency.py --batch 8 --input-len 512 --output-len 128   --output-json latency-8-512-128.json
-CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_latency.py --batch 8 --input-len 512 --output-len 1024  --output-json latency-8-512-1024.json
-CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_latency.py --batch 8 --input-len 2048 --output-len 128  --output-json latency-8-2048-128.json
-CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_latency.py --batch 8 --input-len 2048 --output-len 1024 --output-json latency-8-2048-1024.json
-CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_latency.py --batch 8 --input-len 4096 --output-len 128  --output-json latency-8-4096-128.json
-CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_latency.py --batch 8 --input-len 4096 --output-len 1024 --output-json latency-8-4096-1024.json
+CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_latency.py --model $model --batch 1 --input-len 512 --output-len 128   --output-json latency-1-512-128.json
+CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_latency.py --model $model --batch 1 --input-len 512 --output-len 1024  --output-json latency-1-512-1024.json
+CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_latency.py --model $model --batch 1 --input-len 2048 --output-len 128  --output-json latency-1-2048-128.json
+CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_latency.py --model $model --batch 1 --input-len 2048 --output-len 1024 --output-json latency-1-2048-1024.json
+CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_latency.py --model $model --batch 1 --input-len 4096 --output-len 128  --output-json latency-1-4096-128.json
+CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_latency.py --model $model --batch 1 --input-len 4096 --output-len 1024 --output-json latency-1-4096-1024.json
 
-CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_latency.py --batch 16 --input-len 512 --output-len 128   --output-json latency-16-512-128.json
-CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_latency.py --batch 16 --input-len 512 --output-len 1024  --output-json latency-16-512-1024.json
-CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_latency.py --batch 16 --input-len 2048 --output-len 128  --output-json latency-16-2048-128.json
-CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_latency.py --batch 16 --input-len 2048 --output-len 1024 --output-json latency-16-2048-1024.json
-CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_latency.py --batch 16 --input-len 4096 --output-len 128  --output-json latency-16-4096-128.json
-CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_latency.py --batch 16 --input-len 4096 --output-len 1024 --output-json latency-16-4096-1024.json
+CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_latency.py --model $model --batch 8 --input-len 512 --output-len 128   --output-json latency-8-512-128.json
+CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_latency.py --model $model --batch 8 --input-len 512 --output-len 1024  --output-json latency-8-512-1024.json
+CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_latency.py --model $model --batch 8 --input-len 2048 --output-len 128  --output-json latency-8-2048-128.json
+CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_latency.py --model $model --batch 8 --input-len 2048 --output-len 1024 --output-json latency-8-2048-1024.json
+CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_latency.py --model $model --batch 8 --input-len 4096 --output-len 128  --output-json latency-8-4096-128.json
+CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_latency.py --model $model --batch 8 --input-len 4096 --output-len 1024 --output-json latency-8-4096-1024.json
+
+CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_latency.py --model $model --batch 16 --input-len 512 --output-len 128   --output-json latency-16-512-128.json
+CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_latency.py --model $model --batch 16 --input-len 512 --output-len 1024  --output-json latency-16-512-1024.json
+CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_latency.py --model $model --batch 16 --input-len 2048 --output-len 128  --output-json latency-16-2048-128.json
+CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_latency.py --model $model --batch 16 --input-len 2048 --output-len 1024 --output-json latency-16-2048-1024.json
+CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_latency.py --model $model --batch 16 --input-len 4096 --output-len 128  --output-json latency-16-4096-128.json
+CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_latency.py --model $model --batch 16 --input-len 4096 --output-len 1024 --output-json latency-16-4096-1024.json
--- benchmarks/vlm_benchmark_throughput.sh
+++ benchmarks/vlm_benchmark_throughput.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
+# 检查参数
+if [ "$#" -ne 1 ]; then
+    echo "usage: bash $0 <测试模型路径>"
+    exit 1
+fi
 root=$(dirname $(dirname $(realpath $0)))
+model=$1
+echo "test model: $model"
+rm -f $root/throughput-*.json
 
-rm $root/throughput-*.json
-CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_throughput.py --num-prompts 1 --input-len 512 --output-len 128   --output-json throughput-1-512-128.json
-CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_throughput.py --num-prompts 1 --input-len 512 --output-len 1024  --output-json throughput-1-512-1024.json
-CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_throughput.py --num-prompts 1 --input-len 2048 --output-len 128  --output-json throughput-1-2048-128.json
-CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_throughput.py --num-prompts 1 --input-len 2048 --output-len 1024 --output-json throughput-1-2048-1024.json
-CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_throughput.py --num-prompts 1 --input-len 4096 --output-len 128  --output-json throughput-1-4096-128.json
-CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_throughput.py --num-prompts 1 --input-len 4096 --output-len 1024 --output-json throughput-1-4096-1024.json
+export VLLM_WORKER_MULTIPROC_METHOD=spawn
 
-CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_throughput.py --num-prompts 8 --input-len 512 --output-len 128   --output-json throughput-8-512-128.json
-CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_throughput.py --num-prompts 8 --input-len 512 --output-len 1024  --output-json throughput-8-512-1024.json
-CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_throughput.py --num-prompts 8 --input-len 2048 --output-len 128  --output-json throughput-8-2048-128.json
-CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_throughput.py --num-prompts 8 --input-len 2048 --output-len 1024 --output-json throughput-8-2048-1024.json
-CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_throughput.py --num-prompts 8 --input-len 4096 --output-len 128  --output-json throughput-8-4096-128.json
-CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_throughput.py --num-prompts 8 --input-len 4096 --output-len 1024 --output-json throughput-8-4096-1024.json
+CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_throughput.py --model $model --num-prompts 1 --input-len 512 --output-len 128   --output-json throughput-1-512-128.json
+CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_throughput.py --model $model --num-prompts 1 --input-len 512 --output-len 1024  --output-json throughput-1-512-1024.json
+CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_throughput.py --model $model --num-prompts 1 --input-len 2048 --output-len 128  --output-json throughput-1-2048-128.json
+CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_throughput.py --model $model --num-prompts 1 --input-len 2048 --output-len 1024 --output-json throughput-1-2048-1024.json
+CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_throughput.py --model $model --num-prompts 1 --input-len 4096 --output-len 128  --output-json throughput-1-4096-128.json
+CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_throughput.py --model $model --num-prompts 1 --input-len 4096 --output-len 1024 --output-json throughput-1-4096-1024.json
 
-CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_throughput.py --num-prompts 16 --input-len 512 --output-len 128   --output-json throughput-16-512-128.json
-CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_throughput.py --num-prompts 16 --input-len 512 --output-len 1024  --output-json throughput-16-512-1024.json
-CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_throughput.py --num-prompts 16 --input-len 2048 --output-len 128  --output-json throughput-16-2048-128.json
-CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_throughput.py --num-prompts 16 --input-len 2048 --output-len 1024 --output-json throughput-16-2048-1024.json
-CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_throughput.py --num-prompts 16 --input-len 4096 --output-len 128  --output-json throughput-16-4096-128.json
-CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_throughput.py --num-prompts 16 --input-len 4096 --output-len 1024 --output-json throughput-16-4096-1024.json
+CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_throughput.py --model $model --num-prompts 8 --input-len 512 --output-len 128   --output-json throughput-8-512-128.json
+CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_throughput.py --model $model --num-prompts 8 --input-len 512 --output-len 1024  --output-json throughput-8-512-1024.json
+CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_throughput.py --model $model --num-prompts 8 --input-len 2048 --output-len 128  --output-json throughput-8-2048-128.json
+CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_throughput.py --model $model --num-prompts 8 --input-len 2048 --output-len 1024 --output-json throughput-8-2048-1024.json
+CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_throughput.py --model $model --num-prompts 8 --input-len 4096 --output-len 128  --output-json throughput-8-4096-128.json
+CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_throughput.py --model $model --num-prompts 8 --input-len 4096 --output-len 1024 --output-json throughput-8-4096-1024.json
+
+CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_throughput.py --model $model --num-prompts 16 --input-len 512 --output-len 128   --output-json throughput-16-512-128.json
+CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_throughput.py --model $model --num-prompts 16 --input-len 512 --output-len 1024  --output-json throughput-16-512-1024.json
+CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_throughput.py --model $model --num-prompts 16 --input-len 2048 --output-len 128  --output-json throughput-16-2048-128.json
+CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_throughput.py --model $model --num-prompts 16 --input-len 2048 --output-len 1024 --output-json throughput-16-2048-1024.json
+CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_throughput.py --model $model --num-prompts 16 --input-len 4096 --output-len 128  --output-json throughput-16-4096-128.json
+CUDA_VISIBLE_DEVICES=0,1 python benchmarks/vlm_benchmark_throughput.py --model $model --num-prompts 16 --input-len 4096 --output-len 1024 --output-json throughput-16-4096-1024.json
